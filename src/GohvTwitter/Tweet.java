3b4055209293433256cae24f5d14ede2d9c90076
gohv <hristovsotirovgeorgi@gmail.com>
2016-06-08 23:41:20 +0300

    Twitter API and config

---
+++ src/GohvTwitter/Tweet.java
@@ -0,0 +1,72 @@
+package GohvTwitter;
+
+import com.sun.xml.internal.bind.v2.TODO;
+import twitter4j.Status;
+import twitter4j.StatusUpdate;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
+/**
+ * build Twitter API - twitter4j
+ */
+
+public class Tweet {
+// Generated from Twitter:
+
+    private final String CONSUMER_KEY = "CRRVcMV2IE4onuZ2l7AT2NJ4b";
+    private final String CONSUMER_SECRET = "fa58YQIetZJ0bGt47zrwH15dne7PEJyxAsdsNGEPLsnZJTiEpN";
+    private final String ACCESS_TOKEN = "2655769404-EZU1CS7xLZGelGYFh1vajZvuJcPogpBC0dRdh2m";
+    private final String ACCESS_TOKEN_SECRET = "epMhSqOvR6IEXG80rHeZBHTcJHi9gRYGO5ZRsNJD3sNsN";
+    private ConfigurationBuilder config = new ConfigurationBuilder();
+    private twitter4j.Twitter twitter;
+
+    public Tweet() throws TwitterException {
+
+        buildConfig();
+
+
+    }
+
+    // initial config for my first twitter app:
+    public void buildConfig() throws TwitterException {
+        config.setDebugEnabled(true);
+        config.setOAuthConsumerKey(CONSUMER_KEY);
+        config.setOAuthConsumerSecret(CONSUMER_SECRET);
+        config.setOAuthAccessToken(ACCESS_TOKEN);
+        config.setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+        // pass the config:
+        TwitterFactory twitterFactory = new TwitterFactory(config.build());
+        // Returns a instance associated with the configuration bound to this factory.
+        twitter = twitterFactory.getInstance();
+    }
+
+    public void tweetStatus(String message) throws TwitterException {
+        StatusUpdate status = new StatusUpdate(message);
+        Status setStatus = twitter.updateStatus(status);
+    }
+
+    public void getFeed() throws TwitterException {
+            // creating a list of Statuses:
+        // TODO : make it readable by my FX GUI:
+        List<Status> tweets = twitter.getHomeTimeline();
+        for (Status s : tweets) {
+            System.out.println(s.getUser().getName() + "------>" + s.getText());
+        }
+    }
+
+    public static void main(String[] args) {
+        Tweet twitter;
+        try {
+            twitter = new Tweet();
+
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
